2d4fed0890e8864982989c6776bb657ffcd7be4f
AkVadim <akseninv@yahoo.com>
2024-11-17 11:38:00 +0300

    Used separate argument for print

---
+++ Module_1_6.py
@@ -0,0 +1,6 @@
+completed_homeworks = 12
+spent_hours = 1.5
+course_name = 'Python'
+average_time = spent_hours/completed_homeworks
+print('Курс:',course_name,', всего задач:', completed_homeworks,', затарачено часов:', spent_hours, ', среднее время выполнения ', average_time,' часа.',sep='')
+
